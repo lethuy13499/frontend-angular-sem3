ddd960585afd7826303ac9c12e0f79ae97cdf249
lethuy13499 <lethuy13499@gmail.com>
2019-09-05 21:06:47 +0700

    5/9/2019

--- src/app/component/admin/role-action/role-action.component.ts
+++ src/app/component/admin/role-action/role-action.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Roleaction } from 'src/app/model/roleAction/RoleAction';
 
 @Component({
   selector: 'app-role-action',
@@ -7,9 +8,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoleActionComponent implements OnInit {
 
-  constructor() { }
+  roleactions: Roleaction[] = [];
+  roleaction: Roleaction = undefined;
+  actionId = '';
+  check: string;
+  ObjFormGroup: FormGroup;
+  roleId: string;
+  get RoleName(): FormControl {
+    return this.ObjFormGroup.get('RoleName') as FormControl;
+  }
+  constructor(private fb: FormBuilder, private http: HttpClient,private myservice:MyserviceService, private router: Router, private activatedRoute: ActivatedRoute,private toastr: ToastrService) {
+    this.router.events.subscribe((event) => {
+      this.myservice.changeMessage('1');
+   });
+  }
+  displayedColumn: string[] = [ 'ActionName', 'Description', 'Action'];
+  dataSource = new MatTableDataSource<Roleaction>(this.roleactions);
+  selection = new SelectionModel<Roleaction>(true, []);
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
 
   ngOnInit() {
+    const RoleId = this.activatedRoute.snapshot.paramMap.get('RoleId');
+
+    this.http.get<string>('http://localhost:65170/api/RoleAction/?RoleId=' + RoleId,{ headers: http() }).subscribe(value => {
+      this.roleaction = JSON.parse(value);
+    });
+
+    this.http.get<string>('http://localhost:65170/api/RoleAction/?RoleId=' + RoleId,{ headers: http() }).subscribe(value => {
+      this.dataSource.data = JSON.parse(value).Data;
+      console.log(this.dataSource.paginator = this.paginator, this.dataSource.sort = this.sort);
+    });
+  }
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+  checkboxLabel(row?: Roleaction): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.ActionId + 1}`;
   }
 
+  listRoleAction() {
+    const RoleId = this.activatedRoute.snapshot.paramMap.get('RoleId');
+    this.http.get<string>('http://localhost:65170/api/RoleAction/?RoleId=' + RoleId,{ headers: http() }).subscribe(value => {
+      this.dataSource.data = JSON.parse(value).Data;
+      console.log(this.dataSource.paginator = this.paginator, this.dataSource.sort = this.sort);
+    });
+  }
+  getId(id) {
+    this.roleId = id;
+  }
+  DeleteRoleAction(idAction: string) {
+    if (confirm('Are you sure you to delete this Action?')) {
+      const RoleId = this.activatedRoute.snapshot.paramMap.get('RoleId');
+      this.http.delete<string>('http://localhost:65170/api/RoleAction/?idAction=' + idAction + '&idRole=' + RoleId,{ headers: http() }).subscribe((res) => {
+        let result = JSON.parse(res);
+        if (result.Success == 1) {
+          this.roleactions = this.roleactions.filter(b => b.ActionId !== this.actionId);
+          this.toastr.success('Delete success!', '');
+          this.listRoleAction();
+        } else {
+          this.toastr.success('Delete fail !', '');
+        }
+
+      });
+    }
+  }
+
+  clickToAddAction() {
+    const RoleId = this.activatedRoute.snapshot.paramMap.get('RoleId');
+    this.router.navigate(['Role/RoleActionAdd/', RoleId]);
+  }
+  clickToGoBack() {
+    this.router.navigate(['Role/']);
+  }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 }
--- src/app/component/admin/user/user.component.ts
+++ src/app/component/admin/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user/users';
+import { MyserviceService } from 'src/app/service/myservice/myservice.service';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,163 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
-
+  users: User[] = [];
+  userId = '';
+  user: User;
+  getUsers: User;
+  constructor(private myservice:MyserviceService, private http: HttpClient, 
+    private router: Router, private fb: FormBuilder,private toar:ToastrService) {
+    this.router.events.subscribe((event) => {
+      this.myservice.changeMessage('1');
+   });
+   }
+   displayedColumn: string[] = ['select', 'UserName', 'FullName', 'Email', 'Phone', 'Status', 'Action'];
+   dataSource = new MatTableDataSource<User>(this.users);
+   selection = new SelectionModel<User>(true, []);
+ 
+   filterForm: FormGroup;
+   @ViewChild(MatPaginator) paginator: MatPaginator;
+   @ViewChild(MatSort) sort: MatSort;
+   listuser() {
+    this.http.get<string>('http://localhost:65170/api/User',{ headers: http() }).subscribe({
+      next: (value) => {
+      this.dataSource.data = JSON.parse(value).Data;
+      console.log(this.dataSource.paginator = this.paginator, this.dataSource.sort = this.sort);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+        alert(err.error.Message);
+      }
+    });
+  }
   ngOnInit() {
+    this.http.get<string>('http://localhost:65170/api/User',{ headers: http() }).subscribe({
+      next:(value) => {
+      this.dataSource.data = JSON.parse(value).Data;
+      console.log(this.dataSource.paginator = this.paginator, this.dataSource.sort = this.sort);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+        alert(err.error.Message);
+      }
+    });
+  }
+  onSearch() {
+    this.http.get<string>('http://localhost:65170/api/User?searchString=' + this.searchString,{ headers: http() }).subscribe(
+      {next:(value) => {
+      this.dataSource.data = JSON.parse(value).Data;
+      console.log(this.dataSource.paginator = this.paginator, this.dataSource.sort = this.sort);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+        alert(err.error.Message);
+      }
+    });
+  }
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+  checkboxLabel(row?: User): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.UserId + 1}`;
+  }
+  onFilter() {
+    const value = this.filterForm.value;
+    console.log(value);
+    this.http.post<string>('http://localhost:65170/api/User/?action=filter', JSON.stringify(value),
+    { headers: http() }).subscribe({
+      next:(value) => {
+        this.dataSource.data = JSON.parse(value).Data;
+    },
+    error: (err: HttpErrorResponse) => {
+      console.log(err);
+      alert(err.error.Message);
+    }
+      });
+  }
+  detail(id) {
+    this.userId = id;
+    console.log(this.userId);
+    this.http.get<string>('http://localhost:65170/api/User/?userid=' + this.userId,{ headers: http() }).subscribe(
+      {next:(value) => {
+      this.getUsers = JSON.parse(value);
+      console.log(this.getUsers);
+    },
+    error: (err: HttpErrorResponse) => {
+      console.log(err);
+      alert(err.error.Message);
+    }
+  });
+    this.http.get<string>('http://localhost:65170/api/User/' + this.userId,{ headers: http() }).subscribe(
+  {
+    next: (value) => {
+      this.user = JSON.parse(value);
+      console.log(value);
+    },
+  error: (err: HttpErrorResponse) => {
+    console.log(err);
+    alert(err.error.Message);
+  }   
+  });
   }
 
+  onDelete(id: string) {
+    if (confirm('Are you sure you want to delete this user?')) {
+      this.http.delete<string>('http://localhost:65170/api/User/' + id, { headers: http() }).subscribe(
+        {next:(res) => {
+        let result = JSON.parse(res);
+        if (result.Success == 1) {
+          this.users = this.users.filter(b => b.UserId !== id); 
+          this.toar.success('success','Delete User');        
+          this.listuser();
+        } else if (result.Success == 0) {
+          this.toar.warning('Error!This user is in use');
+        } else {
+          this.toar.warning('Fail','Delete User');
+        }
+      },
+      error: (err: HttpErrorResponse) => {
+      console.log(err);
+      alert(err.error.Message);
+      }   
+      });
+    }
+  }
+
+  removeSelectedRows(id: string) {
+    if (confirm('Delete selected?')) {
+      this.selection.selected.forEach(item => {
+        this.http.delete<string>('http://localhost:65170/api/User/' + item.UserId,{ headers: http() }).subscribe(
+         {
+          next:(res) => {
+            let result = JSON.parse(res);
+            if (result.Success == 1) {
+              this.dataSource.data = this.dataSource.data.filter(b => b.UserId !== item.UserId);
+              this.toar.success('success','Delete User');     
+              this.listuser();
+            } else if (result.Success == 0) {
+              this.toar.warning('Error!This user is in use');
+            } else {
+              this.toar.warning('Fail','Delete User');
+            }
+          },
+          error: (err: HttpErrorResponse) => {
+            console.log(err);
+            alert(err.error.Message);
+          }
+         } 
+        );
+      });
+      this.selection = new SelectionModel<User>(true, []);
+    }
+  }
 }
